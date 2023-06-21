0f1b44e7221b9cec5d32deebc9145a24a5b0a4fb
izzul-ali <izzulmaali3@gmail.com>
2023-06-21 21:40:36 +0700

    chore: setup express app

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,19 @@
-console.log('hello world');
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+
+const client = process.env.CLIENT_ORIGIN;
+const port = process.env.PORT;
+
+const app = express();
+
+app.use(
+  cors({
+    origin: client,
+    credentials: true,
+    methods: 'GET,PUT,POST,DELETE',
+  })
+);
+app.use(express.json());
+
+app.listen(parseInt(port!), 'localhost', () => console.log(`server running on http://localhost:${port}`));
