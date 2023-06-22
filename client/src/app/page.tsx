9593143c821013d36a0082c7e44ecb3fe87219cb
izzul-ali <izzulmaali3@gmail.com>
2023-06-22 09:25:18 +0700

    feat: update form

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,14 +1,9 @@
-import LoginForm from '~/components/Form';
+import AuthForm from '~/components/form/Auth';
 
 export default function Home() {
   return (
     <main className="h-screen flex flex-col items-center gap-y-10 pt-20 font-primary">
-      <div className="px-5 text-center">
-        <h1 className="font-bold text-3xl text-gray-700">CHAT APP</h1>
-        <p className="text-sm font-semibold text-gray-500">Built with Next.js, Tailwind CSS and Node.js as Server</p>
-      </div>
-
-      <LoginForm />
+      <AuthForm />
     </main>
   );
 }
