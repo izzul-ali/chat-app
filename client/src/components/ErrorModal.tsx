801201849d52e06ee7f37af73ed6eda08700de96
izzul-ali <izzulmaali3@gmail.com>
2023-06-23 08:58:08 +0700

    feat: use the error modal to get errors everywhere

--- client/src/components/ErrorModal.tsx
+++ client/src/components/ErrorModal.tsx
@@ -1,11 +1,19 @@
 'use client';
 
-import { Dispatch, SetStateAction } from 'react';
+import { Dispatch, ReactNode, SetStateAction, createContext, useContext, useEffect, useState } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
 
-export default function ErrorModal({ message, clear }: { message: string; clear: Dispatch<SetStateAction<string>> }) {
+function ErrorModal({ message, clear }: { message: string; clear: Dispatch<SetStateAction<string>> }) {
   return (
-    <div className="absolute right-0 left-0 bg-transparent top-4 z-10">
-      <div className="relative mx-auto w-4/5 py-2 px-6 rounded-md shadow-md shadow-gray-300 bg-white hover:bg-gray-100 text-sm font-primary font-semibold text-red-600">
+    <motion.div
+      key="error-modal"
+      initial={{ top: -5 }}
+      animate={{ top: 15 }}
+      exit={{ top: -5, opacity: 0 }}
+      transition={{ duration: 0.3, type: 'spring' }}
+      className="absolute flex justify-center sm:justify-end sm:pl-10 right-0 left-0 bg-transparent z-10"
+    >
+      <div className="relative w-[90%] sm:w-1/2 lg:w-1/3 xl:w-1/4 py-3 px-6 rounded-md shadow-md shadow-gray-300 bg-white hover:bg-gray-100 text-sm font-primary font-semibold text-red-500">
         <p className="text-center">Error: {message}</p>
         <button
           onClick={() => clear('')}
@@ -14,6 +22,42 @@ export default function ErrorModal({ message, clear }: { message: string; clear:
           X
         </button>
       </div>
-    </div>
+    </motion.div>
   );
 }
+
+interface IErrModalContext {
+  setError: Dispatch<SetStateAction<string>>;
+}
+
+const ErrorModalContext = createContext<IErrModalContext | null>(null);
+
+function ErrorModalProvider({ children }: { children: ReactNode }) {
+  const [error, setError] = useState<string>('');
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setError('');
+    }, 3000);
+
+    return () => clearTimeout(timeout);
+  }, [error]);
+
+  return (
+    <ErrorModalContext.Provider value={{ setError }}>
+      <AnimatePresence>{error && <ErrorModal message={error} clear={setError} />}</AnimatePresence>
+      {children}
+    </ErrorModalContext.Provider>
+  );
+}
+
+export function useErrorModal() {
+  const errContext = useContext(ErrorModalContext);
+  if (!errContext) {
+    throw new Error('Error context provider is not define');
+  }
+
+  return errContext;
+}
+
+export default ErrorModalProvider;
