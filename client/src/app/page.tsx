4b8c9f7a2bf0fddcabbefdb7dde7ecfb242de255
izzul-ali <izzulmaali3@gmail.com>
2023-06-29 22:04:19 +0700

    feat: add check if user is authenticated

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,16 +1,12 @@
 import { getServerSession } from 'next-auth';
-import { RedirectType } from 'next/dist/client/components/redirect';
-import { redirect } from 'next/navigation';
 import LoginAuthentication from '~/components/Auth';
 
 export default async function Home() {
   const session = await getServerSession();
-  if (session?.user) {
-    redirect('/chat', RedirectType.replace);
-  }
+
   return (
-    <main className="h-screen flex flex-col items-center gap-y-10 pt-20 font-primary">
-      <LoginAuthentication />
+    <main className="h-screen flex flex-col items-center font-primary">
+      <LoginAuthentication isAuthenticate={session !== null} />
     </main>
   );
 }
