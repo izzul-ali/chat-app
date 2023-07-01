a5fccc9250edc77089047737766ddc2cae48d18c
izzul-ali <izzulmaali3@gmail.com>
2023-07-01 17:16:13 +0700

    chore: move checking server side session to page

--- client/src/app/chat/layout.tsx
+++ client/src/app/chat/layout.tsx
@@ -1,12 +1,3 @@
-import { getServerSession } from 'next-auth';
-import { RedirectType } from 'next/dist/client/components/redirect';
-import { redirect } from 'next/navigation';
-
 export default async function ChatLayout({ children }: { children: React.ReactNode }) {
-  const session = await getServerSession();
-  if (!session?.user) {
-    redirect('/', RedirectType.replace);
-  }
-
-  return <>{children}</>;
+  return <main className="h-screen w-full relative flex font-primary">{children}</main>;
 }
