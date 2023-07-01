b662be429378f025f804eb8f2348e7371ab3c67b
izzul-ali <izzulmaali3@gmail.com>
2023-07-01 17:13:16 +0700

    chore: add session user type

--- client/src/types/next-auth.d.ts
+++ client/src/types/next-auth.d.ts
@@ -1,4 +1,4 @@
-import NextAuth from 'next-auth';
+import NextAuth, { DefaultSession } from 'next-auth';
 import { UserResponse } from './user';
 
 declare module 'next-auth' {
@@ -6,4 +6,9 @@ declare module 'next-auth' {
    * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
    */
   interface User extends UserResponse {}
+  interface Session {
+    user: {
+      id: string;
+    } & DefaultSession['user'];
+  }
 }
--- client/src/types/user.ts
+++ client/src/types/user.ts
@@ -2,10 +2,17 @@ export interface User {
   id: string;
   name: string;
   email: string;
-  password: string;
   provider: string;
   image: string;
 }
+
+export type SessionUser = {
+  id: string;
+} & {
+  name?: string | null | undefined;
+  email?: string | null | undefined;
+  image?: string | null | undefined;
+};
 
 export interface UserResponse {
   id: string;
