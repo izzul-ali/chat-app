03b2e19ea1c9d790e930723275192b32dd3ad08c
izzul-ali <izzulmaali3@gmail.com>
2023-07-02 22:15:33 +0700

    style: add custom colors

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
+  darkMode: 'class',
   theme: {
     extend: {
       fontFamily: {
         primary: ['var(--font-red-hat)'],
+      },
+      colors: {
+        menu: '#fafcff',
+        profile: '#fafcff',
+        sidebar: '#f6f8fb',
+        send: '#fafcff',
+        'message-header': '#fafcff',
+        'message-list': '#f6f8fb',
+        'input-message': '#f6f8fb',
       },
     },
   },
