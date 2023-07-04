240a1dc66988827ff6111c1cd217af1ccc89769d
izzul-ali <izzulmaali3@gmail.com>
2023-07-04 11:14:56 +0700

    chore: add multiple remote pattern domains

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   poweredByHeader: false,
+  reactStrictMode: false,
+  experimental: {
+    serverActions: true,
+  },
+  images: {
+    dangerouslyAllowSVG: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.pngall.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
