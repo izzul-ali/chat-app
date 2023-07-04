f82006d8aa0e8a6533a10200a8a559f5e77ce39b
izzul-ali <izzulmaali3@gmail.com>
2023-07-04 11:21:06 +0700

    sytle: add color on dark mode

--- client/src/components/ErrorModal.tsx
+++ client/src/components/ErrorModal.tsx
@@ -13,11 +13,11 @@ function ErrorModal({ message, clear }: { message: string; clear: Dispatch<SetSt
       transition={{ duration: 0.3, type: 'spring' }}
       className="absolute flex justify-center sm:justify-end sm:pr-5 w-full bg-transparent z-10"
     >
-      <div className="relative w-[90%] sm:w-1/2 lg:w-1/3 xl:w-1/4 py-3 px-6 rounded-md shadow-md shadow-gray-300 bg-white hover:bg-gray-100 text-sm font-primary font-semibold text-red-500">
+      <div className="relative w-[90%] sm:w-1/2 lg:w-1/3 xl:w-1/4 py-3 px-6 rounded-md bg-gray-100 dark:bg-gray-800 text-sm font-primary font-semibold text-red-500">
         <p className="text-center">Error: {message}</p>
         <button
           onClick={() => clear('')}
-          className="absolute -right-2 -top-2 py-1 px-2 rounded-full bg-gray-100 text-xs text-gray-800"
+          className="absolute -right-2 -top-2 py-1 px-2 rounded-full bg-gray-50 dark:bg-gray-800 text-xs text-gray-800 dark:text-gray-200"
         >
           X
         </button>
