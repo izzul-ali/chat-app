42dfb1be7f71e8cbc6e112bb511a1b20417295cc
izzul-ali <izzulmaali3@gmail.com>
2023-07-04 11:17:23 +0700

    chore: executes logout when it receives a 401 status code response

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -1,7 +1,22 @@
-import axios from 'axios';
+import axios, { AxiosError, AxiosRequestConfig, AxiosResponse, InternalAxiosRequestConfig } from 'axios';
+import { handleLogout } from './auth/signin';
 
 const axiosInstance = axios.create({
+  withCredentials: true,
   baseURL: 'http://localhost:8080/api',
 });
+
+axiosInstance.interceptors.response.use(
+  (resp: AxiosResponse) => {
+    return Promise.resolve(resp);
+  },
+  async (error: AxiosError) => {
+    if (error.response?.status === 401) {
+      return await handleLogout();
+    }
+
+    return Promise.reject(error);
+  }
+);
 
 export default axiosInstance;
