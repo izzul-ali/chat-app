2cafd2ff6c3850eb255691c237161a6dce054f64
izzul-ali <izzulmaali3@gmail.com>
2023-07-04 10:55:26 +0700

    chore: add stirct type to message status

---
+++ server/src/types/message.ts
@@ -0,0 +1,22 @@
+import { User } from './user';
+
+export type MessageType = 'TEXT' | 'FILE' | 'IMAGE';
+type MessageStatus = 'SENT' | 'READ';
+
+export interface Message {
+  id?: number;
+  senderId: string;
+  receiverId: string;
+  type: MessageType;
+  status: MessageStatus;
+  message?: string;
+  urlFileOrImage?: string;
+  createdAt: Date;
+}
+
+export interface AllContactMessage extends User {
+  type?: MessageType;
+  urlFileOrImage?: string;
+  currentMessage?: string;
+  createdAt?: Date;
+}
