c08cb3357d4b73d924504e8f9749039e2153dd8f
izzul-ali <izzulmaali3@gmail.com>
2023-07-05 13:51:57 +0700

    feat: improve body email verification

--- server/src/service/verification.ts
+++ server/src/service/verification.ts
@@ -52,10 +52,73 @@ export default async function sendRegisterEmailVerification(userEmail: string, i
 
 function createMailOptions(email: string, link: string) {
   const body = `
-  <h1>Hello ${email}</h1>
-  <p>This is verification link to access my chat app</p>
-  <p>Please click url bellow to confirm verification</p>
-  <a href="${link}">Verification</a>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>Verify | Chat App</title>
+  <style>
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  } 
+  
+  .title {
+    font-size: 24px;
+    font-weight: bold;
+    margin-bottom: 35px;
+  }
+  
+  .desc {
+    font-size: 15px;
+  }
+  
+  .btn-link {
+    text-decoration: none;
+    font-size: 14px;
+    font-weight: 500;
+    outline: none;
+    padding: 5px 28px;
+    border: none;
+    border-radius: 3px;
+    background-color: rgb(23, 139, 255);
+    color: azure;
+    cursor: pointer;
+    transition: all;
+    transition-duration: 0.2s;
+  }
+  
+  .btn-link:hover {
+    background-color: rgb(0, 128, 255);
+  }
+  
+  .separate {
+    width: 0px;
+    padding: 1px 20px;
+    display: block;
+    border: none;
+    border-radius: 5px;
+    background-color: rgb(228, 228, 228);
+    margin-top: 40px;
+  }
+  
+  .author {
+    margin-top: 10px;
+    font-weight: 600;
+    color: rgb(194, 194, 194);
+  }
+  </style>
+  </head>
+
+  <body>
+      <h1 class="title">Signin Verification for Chat App</h1>
+      <a href='${link}' target='_blank' rel='noreferrer' class="btn-link">verify</a>
+      <p class="desc">Hello , This link only be valid for the next 5 minutes.</p>
+
+      <span class="separate"></span>
+      <p class="author">Izzul</p>
+  </body>
+</html>
     `;
 
   return {
